6120805f56fe349d7bb8adcbab488f204cdeb98e
Berdishukur <boboqulovberdishukur@gmail.com>
2023-07-23 21:27:54 +0500

    Update models

--- adminapp/models.py
+++ adminapp/models.py
@@ -2,6 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+class Country(models.Model):
+    name=models.CharField(max_length=100,null=False,blank=False)
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    country=models.ForeignKey(Country,on_delete=models.SET_NULL,null=True)
+    def __str__(self):
+        return self.name
+class Unversity(models.Model):
+    name=models.CharField(max_length=100,null=False,blank=False)
+    region=models.ForeignKey(Region,on_delete=models.SET_NULL,null=True)
+    def __str__(self):
+        return self.name
 class Faculty(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
 
